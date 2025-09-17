d5e929299dfdf756f0e3fe050e26c0e11ef76583
LiuWei <183074632@qq.com>
2025-09-17 14:26:08 +0800

    style: cancel pylint check for s7_plc.py

--- siemens_plc/s7_plc.py
+++ siemens_plc/s7_plc.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 """PLC drive."""
 import logging
 import os
@@ -13,7 +14,6 @@ from snap7 import util
 from siemens_plc.exception import PLCReadError, PLCWriteError
 
 
-# pylint: disable=too-many-arguments, too-many-positional-arguments, too-many-instance-attributes
 class S7PLC:
     """This class provides methods for interacting with a Siemens S7 PLC using the Snap7 library."""
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
